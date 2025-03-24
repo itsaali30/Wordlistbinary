807fc878a04916b43672b5001aba1db24516a561
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:14:16 +0530

    Update BuySell.vue

--- 1/src/components/Wallet/BuySell.vue
+++ 1/src/components/Wallet/BuySell.vue
@@ -1,1 +1,18 @@
 
+<template>
+  <div>
+    <h2>Buy / Sell Coins</h2>
+    <input v-model="amount" type="number" placeholder="Enter amount" />
+    <button @click="buy">Buy</button>
+    <button @click="sell">Sell</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const amount = ref(0);
+
+const buy = () => alert(`Bought ${amount.value} coins!`);
+const sell = () => alert(`Sold ${amount.value} coins!`);
+</script>

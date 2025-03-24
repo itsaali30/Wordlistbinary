ebe72bc8324d41f0b72d56c1941618eb5428b8b7
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:17:09 +0530

    Update SendFriend.vue

--- 1/src/components/SendFriend.vue
+++ 1/src/components/SendFriend.vue
@@ -1,1 +1,22 @@
 
+<template>
+  <div>
+    <h2>Send Coins to a Friend</h2>
+    <input v-model="friendId" placeholder="Friend ID" />
+    <input v-model="email" placeholder="Friend Email" />
+    <input v-model="pin" type="password" placeholder="Confirm PIN" />
+    <button @click="sendCoins">Send Coins</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const friendId = ref('');
+const email = ref('');
+const pin = ref('');
+
+const sendCoins = () => {
+  alert(`Coins sent to ${friendId.value} - ${email.value}`);
+};
+</script>

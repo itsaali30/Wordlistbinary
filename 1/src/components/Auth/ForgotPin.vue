e20566d8dc57ed4d9f9e20ffc063e62f72d3b191
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:13:21 +0530

    Update ForgotPin.vue

--- 1/src/components/Auth/ForgotPin.vue
+++ 1/src/components/Auth/ForgotPin.vue
@@ -1,1 +1,23 @@
 
+<template>
+  <div>
+    <h2>Forgot PIN</h2>
+    <input v-model="email" placeholder="Enter your registered email" />
+    <button @click="resetPin">Reset PIN</button>
+    <router-link to="/login">Back to Login</router-link>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+
+const resetPin = () => {
+  if (email.value) {
+    alert(`PIN reset link sent to ${email.value}`);
+  } else {
+    alert("Please enter your registered email.");
+  }
+};
+</script>

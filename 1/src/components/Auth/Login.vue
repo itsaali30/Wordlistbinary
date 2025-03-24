20fb5266d76ba598cb04a3bdf7e11903f206aa70
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 03:54:29 +0530

    Update Login.vue

--- 1/src/components/Auth/Login.vue
+++ 1/src/components/Auth/Login.vue
@@ -1,1 +1,29 @@
-1
+
+<template>
+  <div>
+    <h2>Login</h2>
+    <input v-model="email" placeholder="Email" />
+    <input v-model="pin" type="password" placeholder="PIN" />
+    <button @click="login">Login</button>
+    <router-link to="/forgot-pin">Forgot PIN?</router-link>
+    <router-link to="/signup">Sign Up</router-link>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const pin = ref('');
+const router = useRouter();
+
+const login = () => {
+  if (email.value === 'test@test.com' && pin.value === '1234') {
+    alert('Login successful');
+    router.push('/wallet');
+  } else {
+    alert('Invalid credentials');
+  }
+};
+</script>

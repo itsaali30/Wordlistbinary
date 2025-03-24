1a54488e48e42b5ef0760d20b8da583694e5b14e
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:16:38 +0530

    Update ReferFriend.vue

--- 1/src/components/ReferFriend.vue
+++ 1/src/components/ReferFriend.vue
@@ -1,1 +1,19 @@
 
+<template>
+  <div>
+    <h2>Refer a Friend</h2>
+    <input v-model="referralCode" placeholder="Referral Code" />
+    <button @click="copyReferral">Copy Referral Link</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const referralCode = ref("REF123456");
+
+const copyReferral = () => {
+  navigator.clipboard.writeText(referralCode.value);
+  alert("Referral link copied!");
+};
+</script>

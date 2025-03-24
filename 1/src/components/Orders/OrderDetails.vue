1e169933cf65258841c6c65c2172d28e8728be55
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:15:26 +0530

    Update OrderDetails.vue

--- 1/src/components/Orders/OrderDetails.vue
+++ 1/src/components/Orders/OrderDetails.vue
@@ -1,1 +1,29 @@
 
+<template>
+  <div>
+    <h2>Order Details</h2>
+    <p>Seller ID: {{ $route.params.id }}</p>
+    <p>Available Coins: 500</p>
+    <p>Price per Coin: $10</p>
+    <input v-model="utr" placeholder="Enter UTR" />
+    <input type="file" @change="uploadProof" />
+    <button @click="submitOrder">Submit Order</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const utr = ref('');
+
+const uploadProof = (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    alert("Proof uploaded!");
+  }
+};
+
+const submitOrder = () => {
+  alert(`Order submitted with UTR: ${utr.value}`);
+};
+</script>

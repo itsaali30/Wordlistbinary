140fd6d3a1841544c4e279f7113638326dd778c3
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:12:30 +0530

    Update Signup.vue

--- 1/src/components/Auth/Signup.vue
+++ 1/src/components/Auth/Signup.vue
@@ -1,1 +1,30 @@
+<template>
+  <div>
+    <h2>Sign Up</h2>
+    <input v-model="email" placeholder="Email" />
+    <input v-model="pin" type="password" placeholder="PIN" />
+    <input v-model="confirmPin" type="password" placeholder="Confirm PIN" />
+    <button @click="signup">Sign Up</button>
+    <router-link to="/login">Already have an account? Login</router-link>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const email = ref('');
+const pin = ref('');
+const confirmPin = ref('');
+const router = useRouter();
+
+const signup = () => {
+  if (pin.value !== confirmPin.value) {
+    alert("PINs do not match!");
+  } else {
+    alert("Sign Up successful! Please log in.");
+    router.push('/login');
+  }
+};
+</script>
 

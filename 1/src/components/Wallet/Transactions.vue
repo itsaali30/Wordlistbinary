170aeaa6ec83bc8bd459979923212953bc131ad4
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 04:14:45 +0530

    Update Transactions.vue

--- 1/src/components/Wallet/Transactions.vue
+++ 1/src/components/Wallet/Transactions.vue
@@ -1,1 +1,19 @@
 
+<template>
+  <div>
+    <h2>Transaction History</h2>
+    <ul>
+      <li v-for="(txn, index) in transactions" :key="index">
+        {{ txn.type }} - {{ txn.amount }} Coins - {{ txn.date }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const transactions = [
+  { type: "Buy", amount: 100, date: "2025-03-25" },
+  { type: "Sell", amount: 50, date: "2025-03-24" },
+  { type: "Buy", amount: 200, date: "2025-03-23" }
+];
+</script>

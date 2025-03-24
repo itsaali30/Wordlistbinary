69a4ceedd09862b702bbe59cdd9038b148902187
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 03:55:28 +0530

    Update WalletHome.vue

--- 1/src/components/Wallet/WalletHome.vue
+++ 1/src/components/Wallet/WalletHome.vue
@@ -1,1 +1,18 @@
+<template>
+  <h2>Wallet Balance: {{ balance }} Coins</h2>
+  <button @click="buy">Buy</button>
+  <button @click="sell">Sell</button>
+  <button @click="viewTransactions">Transactions</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const balance = 500;
+const router = useRouter();
+
+const buy = () => router.push('/wallet/buy-sell');
+const sell = () => router.push('/wallet/buy-sell');
+const viewTransactions = () => router.push('/wallet/transactions');
+</script>
 

b5e46a9b5571fedb48b7bd13187f9bb3ccd6493e
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2025-03-25 03:52:02 +0530

    Update App.vue

--- 1/src/App.vue
+++ 1/src/App.vue
@@ -1,1 +1,16 @@
 
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  margin: 20px;
+}
+</style>
